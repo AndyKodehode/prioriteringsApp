9ebe07b9a1a53c7e9a8d7ccfa95117a870885e75
AndyKodehode <andykodehode@gmail.com>
2025-01-06 20:46:40 +0100

    øke og minske tall

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,20 @@
+<template> 
+  <div > 
+    <h1>{{title}}</h1>
+    <button @click="number++"> Øke</button>
+    <button @click="number--"> Minske</button>
+    <h2>{{number}}</h2>
+  </div>
+</template>
 
+<script>
+export default {
+    name: "App",
+    data(){
+        return{
+            title:'Anders og Lars sin app',
+            number: 0
+        }
+    }
+}
+</script>
