f6949ff9d4aaebcb9406684b983a700a938c1bab
AndyKodehode <andykodehode@gmail.com>
2025-01-07 20:32:12 +0100

    prosent og ferdig oppsett for slider

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,18 @@
       <h3>{{shortTermValue}}</h3>
    </div>
 
+   <div > 
+      <label for="Langsiktig">Lang sikt</label>
+      <input class="slider" id="Langsiktig"  @input="handleLongTerm(longTermValue)" type="range" min="0" max="5" v-model="longTermValue">
+      <h3>{{longTermValue}}</h3>
+   </div>
+
+   <div > 
+      <label for="Karriere">Karriere</label>
+      <input class="slider" id="Karriere"  @input="handleCareer(careerValue)" type="range" min="0" max="5" v-model="careerValue">
+      <h3>{{careerValue}}</h3>
+   </div>
+
    <button @click="showHideAnswer" >result</button>
 
    <div v-if="isVisible" >
@@ -36,14 +48,16 @@
        </li>
 
        <li>
-
+        {{longTermMessage}}
        </li>
 
        <li>
-
+        {{careerMessage}}
        </li>
 
     </ul>
+
+    <h2>Anbefaling for kjøp: {{precent}} %</h2>
 
    </div>
 </div>
@@ -67,14 +81,15 @@ export default {
             longTermMessage: "",
             careerMessage: "",
 
-            isVisible: false
+            isVisible: false,
+            precent: 0
         }
     },
 
     methods: {
       handleEnergyChange(value){
      
-      this.energyValueValue = value;
+      this.energyValue = value;
 
       if(this.energyValue <= 1){
         this.energyMessage = "Dette krever mye! Kanskje bedre gjøre noe som gir deg mer energi?"
@@ -124,9 +139,53 @@ export default {
          }
       },
 
+      handleLongTerm(value){
+
+        this.longTermValue = value
+
+        if(this.longTermValue <= 1){
+            this.longTermMessage = "Gir null niks nada i framtidig avkastning. (men det er gøy da?)"
+         } else if( this.longTermValue == 2){
+          this.longTermMessage = "Dette var litt vel kortsiktig?"
+         }else if( this.longTermValue == 3){
+          this.longTermMessage = "Kanskje du får litt tilbake, men det er jo ikke helt sikkert da…"
+         }else if( this.longTermValue == 4){
+          this.longTermMessage = "Her får du en del tilbake igjen på lang sikt"
+         }else if( this.longTermValue == 5){
+          this.longTermMessage = "Investering!!!"
+         }
+      },
+
+
+      handleCareer(value){
+
+        this.careerValue = value
+
+        if(this.careerValue <= 1){
+            this.careerMessage = "Blir ingen professor av dette. Spist i dag, glemt i morra."
+         } else if( this.careerValue == 2){
+          this.careerMessage = "Havner utenfor CV’en"
+         }else if( this.careerValue == 3){
+          this.careerMessage = "Kanskje du blir litt klokere?"
+         }else if( this.careerValue == 4){
+          this.careerMessage = "Dette kan du skrive om på Linked-in"
+         }else if( this.careerValue == 5){
+          this.careerMessage = "Dannelse!!"
+         }
+      },
+
       showHideAnswer(){
           this.isVisible = !this.isVisible
-      }
+
+          let total = parseFloat(this.energyValue) + parseFloat(this.needOfOhterValue) + parseFloat(this.shortTermValue)+ parseFloat(this.longTermValue) + parseFloat(this.careerValue)
+
+          let precentege = (total/ 25) * 100
+
+          this.precent = precentege
+
+      }, 
+
+      
     }
 }
 </script>
