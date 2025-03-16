2cfffa38edeb0816e2577b810d914e638fdbf008
AndyKodehode <andykodehode@gmail.com>
2025-03-16 19:12:46 +0100

    pwa kode

--- src/main.js
+++ src/main.js
@@ -5,3 +5,12 @@ import App from './App.vue'
 
 
 createApp(App).mount('#app')
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js')
+        .then(reg => console.log('Service Worker registered:', reg))
+        .catch(err => console.error('Service Worker registration failed:', err));
+    });
+  }
+  
