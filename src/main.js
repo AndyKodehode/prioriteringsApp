0854345123d48340fcf294c0226a41980cb6421d
AndyKodehode <andykodehode@gmail.com>
2025-03-24 21:11:15 +0100

    link i main

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ createApp(App).mount('#app')
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js')
+      navigator.serviceWorker.register('/dev-dist/sw.js')
         .then(reg => console.log('Service Worker registered:', reg))
         .catch(err => console.error('Service Worker registration failed:', err));
     });
